478a8b8dca630992f6a0b303526f148d351d8ce9
Jim Klo <jim.klo@sri.com>
2013-11-22 15:15:53 -0800

    updated data generator
    Signed-off-by: Jim Klo <jim.klo@sri.com>

--- commoncore/make_tree_control_data.js
+++ commoncore/make_tree_control_data.js
@@ -1,18 +1,30 @@
 var fs = require("fs"),
     _ = require("underscore")._;
 
+var commonjs_map = "../lib/gen/standards.js";
 
 var work_files = [
     {
+        label: "Common Core State Standards for English Language Arts",
         source: "./ccss_ela_D10003FC.json",
-        target: "../js/data/ccss_ela.json"
+        target: "../js/data/ccss_ela.json",
     },
     {
+        label: "Common Core State Standards for Math",
         source: "./ccss_math_D10003FB.json",
         target: "../js/data/ccss_math.json"
     }
 
 ];
+
+var all_exports = { 
+    map: {},
+    data: [],
+    frameworks:[]
+};
+
+var current_framework = -1;
+
 
 var generated_warning = fs.readFileSync("./warning_template.js").toString();
 var node_template = fs.readFileSync("./node_template.html").toString();
@@ -21,12 +33,18 @@ function processContents(data) {
     var raw_json = JSON.parse(data),
         tree_data = makeFancyTreeData(raw_json);
 
-
-
     console.log(JSON.stringify(tree_data, null, 4));
     return tree_data;
 }
 
+
+function addMapping(nodedata) {
+    if (!nodedata.data.dotnotation)
+        return;
+    var obj = {dotnotation: nodedata.data.dotnotation, uri: nodedata.key, fw: current_framework};
+    var len = all_exports.data.push(obj);
+    all_exports.map[obj.uri] = all_exports.map[obj.dotnotation] = len-1;
+}
 
 
 function makeFancyTreeData(current_set) {
@@ -64,13 +82,12 @@ function makeFancyTreeData(current_set) {
         var nodedata = {
             title: _.template(node_template, {dotnotation: std_dotnotation, text: std_text}).trim(),
             key: std_uri,
-            // hideCheckbox: false,
-            // unselectable: (!std_dotnotation),
             data: {
                 dotnotation: std_dotnotation,
                 edlevel: std_grade_level
             }
         }
+        addMapping(nodedata);
         
         if (parent.children && parent.children.length>0) {
             nodedata.children = makeFancyTreeData(parent.children);
@@ -84,12 +101,25 @@ function makeFancyTreeData(current_set) {
 }
 
 
-_.each(work_files, function(work){
+_.each(work_files, function(work, index){
     
     fs.readFile(work.source, null, function(err, data){
+        current_framework = all_exports.frameworks.push(work.label) - 1;
+
         var processed = processContents(data);
 
-        fs.writeFileSync(work.target, JSON.stringify(processed, null, 4));
-    });
+        fs.writeFile(work.target, JSON.stringify(processed, null, 4), null, function(err){
+            console.log("Index:"+index);
+            console.log("length:"+work_files.length);
+            if (index==work_files.length-1) {
+                fs.writeFile(commonjs_map, _.template(generated_warning, {exports:all_exports}), function(err){
+                    if (err)
+                        console.log(err);
+                    else
+                        console.log("wrote file");
+                });
+            }
+        });
 
+    });
 });
--- commoncore/warning_template.js
+++ commoncore/warning_template.js
@@ -1,4 +1,14 @@
 /**********************************************************************/
 /** WARNING: DO NOT EDIT FILE DIRECTLY IT IS AUTOMATICALLY GENERATED **/
 /**********************************************************************/
-
+(function() {
+    var stds = <%=JSON.stringify(exports, null, 4)%>;
+    exports.find = function(key) {
+        if (stds.map[key]!==undefined) {
+            var std = _.clone(stds.data[stds.map[key]]);
+            
+            return _.extend(std, { fw: stds.frameworks[std.fw] });
+        }
+        return null;
+    }
+})();
